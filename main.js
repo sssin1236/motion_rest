cdd72ab671f77a68831e56f2dbac1adb33c105e9
sssin1236 <suj0358@gmail.com>
2021-11-07 02:10:36 +0900

    style 수정

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(".rolling article").on("click", function(){
     $(this).appendTo(".rolling");
+    $(".rolling").css({ transform: translateX("-120px")})
 
     let vid = $(this).find("video").attr("src");
     console.log(vid);
