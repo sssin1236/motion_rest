ebbdb87fb33062bdab81ad53bb9b546761b41a6d
sssin1236 <suj0358@gmail.com>
2021-11-08 01:16:03 +0900

    script 삽입

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-$(".rolling article").on("click", function(){
-    $(this).appendTo(".rolling");
-    $(".rolling").css({ transform: translateX("-120px")})
+$(".textBox>article").on("click", function(){
+    $(this).appendTo(".textBox");
 
     let vid = $(this).find("video").attr("src");
     console.log(vid);
