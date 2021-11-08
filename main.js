ab6c66ce2b036550d506184b23a0be4fdbe532c7
sssin1236 <suj0358@gmail.com>
2021-11-08 17:16:17 +0900

    틀 완성

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(".textBox>article").on("click", function(){
     $(this).appendTo(".textBox");
+    $(".rolling>article").first().appendTo(".rolling");
 
     let vid = $(this).find("video").attr("src");
     console.log(vid);
